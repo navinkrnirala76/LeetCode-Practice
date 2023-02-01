3e94c67ca9b0a5f3113b03c459d9cf8157581db2
Navin Kumar <73814698+navinkrnirala76@users.noreply.github.com>
2023-02-01 09:45:59 +0530

    Create Valid Parenthesis.cpp

---
+++ Valid Parenthesis.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+            stack<char>st;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('|| s[i]=='{'|| s[i]=='['){
+                st.push(s[i]);
+            }else{
+                if(st.empty() )
+                    return false;
+                 if(s[i]==')' && st.top()!='(' ) 
+                    return false;
+                else if(s[i]=='}' && st.top()!='{' ) 
+                    return false;
+                else if(s[i]==']' && st.top()!='[' )
+                    return false;
+                
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
