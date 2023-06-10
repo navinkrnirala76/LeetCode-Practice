655ce162a6ef2f5cdf17ff95a238fd2b92292232
Navin Kumar <73814698+navinkrnirala76@users.noreply.github.com>
2023-06-10 23:35:49 +0530

    Create Add Two Numbers.cpp

---
+++ Add Two Numbers.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummyHead = new ListNode(0);
+        ListNode* curr = dummyHead;
+        int carry = 0;
+
+        while (l1 || l2) {
+            int sum = carry;
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+        }
+
+        if (carry > 0) {
+            curr->next = new ListNode(carry);
+        }
+
+        return dummyHead->next;
+    }
+};
