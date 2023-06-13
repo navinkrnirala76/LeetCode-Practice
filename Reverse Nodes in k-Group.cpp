8e77dfd87c5fd4b1b7b3d72ffd421939fd58695f
Navin Kumar <73814698+navinkrnirala76@users.noreply.github.com>
2023-06-13 11:18:17 +0530

    Create Reverse Nodes in k-Group.cpp

---
+++ Reverse Nodes in k-Group.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        int cnt = 0;
+        ListNode* counter = head;
+        ListNode* temp = head;
+        ListNode* ans = head;
+        while(counter!=NULL){
+            cnt++;
+            counter = counter->next;
+        }
+        cnt /= k;
+        stack<int> st;
+        int k1 = k;
+
+        while(temp!=NULL){
+            st.push(temp->val);
+            k1--;
+            temp = temp->next;
+            if(k1==0){
+                while(!st.empty()){
+                    ans->val = st.top();
+                    st.pop();
+                    ans = ans->next;
+                }
+                cnt--;
+                k1 = k;
+            }
+            if(cnt==0){
+                break;
+            }
+        }
+        return head;
+    }
+};
