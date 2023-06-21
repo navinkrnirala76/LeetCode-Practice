43120e7cc02d08b19778b37638eea9a2dbde24a5
Navin Kumar <73814698+navinkrnirala76@users.noreply.github.com>
2023-06-21 10:09:43 +0530

    Update Climibing Stairs.cpp

--- Climibing Stairs.cpp
+++ Climibing Stairs.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n==0){
-            return 0;
-        } else if (n==1){
-            return 1;
-        }
-        int first = 1 ;
-        int second = 1;
-        int ans;
-        for(int i =2;i<=n; i++){
-            ans = first + second;
-            first = second ;
-            second = ans;
-        }
-        return ans;
+        if(n<=2)
+         return n;
+        vector<int> dp(n+1);
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++)
+         dp[i]=dp[i-1]+dp[i-2];
+        
+        return dp[n];
     }
 };
