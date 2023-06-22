48b9c4ea92551355353fc2c80d26829b3c357101
Navin Kumar <73814698+navinkrnirala76@users.noreply.github.com>
2023-06-22 10:06:59 +0530

    Update Subsets.cpp

--- Subsets.cpp
+++ Subsets.cpp
@@ -1,29 +1,61 @@
 class Solution {
 public:
-    
-    void sub(int i,vector<int> &nums, vector<int> &temp,vector<vector<int>>&ans)
-    {
-        if(i>=nums.size())
-        {
-            ans.push_back(temp);
+    //RECURSION - STRIVER
+    void f(int index,vector<int>& nums,vector<int>& ds,vector<vector<int>>& ans){
+        int n=nums.size();
+        if(index>=n){
+            ans.push_back(ds);
             return;
         }
-        
-        // pick
-        temp.push_back(nums[i]);
-        sub(nums, i+1, temp,ans);
-
-        // not pick
-        temp.pop_back();
-        //this pop is necessary because we are passing temp by reference. 
-        //if anything will be picked in above call it will remain in temp if we will not pop it.
-        sub(nums, i+1, temp,ans);
-    }
-    
-    vector<vector<int>> subsets(vector<int>& nums) {
+        f(index+1,nums,ds,ans);
+        ds.push_back(nums[index]);
+        f(index+1,nums,ds,ans);
+        ds.pop_back();
+        return;
+    } 
+    vector<vector<int>> subsets(vector<int>& nums){
         vector<vector<int>> ans;
-        vector<int> temp;       
-        sub(nums, 0, temp,ans); 
+        vector<int> ds;
+        f(0,nums,ds,ans);
         return ans;
-    }
+     }
+
+
+    //POWER SET - STRIVER
+    // vector<vector<int>> subsets(vector<int>& nums){
+    //     vector<vector<int>> TotalSubsets;
+    //     int n=nums.size();
+    //     for(int num=0;num<=pow(2,n)-1;num++){
+    //         vector<int> sub;
+    //         for(int i=0;i<=n-1;i++){
+    //             if(num&(1<<i)){
+    //                 sub.push_back(nums[i]);
+    //             }
+    //         }
+    //         TotalSubsets.push_back(sub);
+    //     }
+    //     return TotalSubsets;
+    // }
+
+
+    //RECURSION - ADITYA VERMA
+    // void solve(vector<int> ip,vector<int> op,vector<vector<int>>& ans){
+    //     if(ip.size()==0){
+    //         ans.push_back(op);
+    //         return;
+    //     }
+    //     vector<int> op1=op;
+    //     vector<int> op2=op;
+    //     op2.push_back(ip[0]);
+    //     ip.erase(ip.begin()+0);
+    //     solve(ip,op1,ans);
+    //     solve(ip,op2,ans);
+    //     return;
+    // }
+    // vector<vector<int>> subsets(vector<int>& nums){
+    //     vector<vector<int>> ans;
+    //     vector<int> op;
+    //     solve(nums,op,ans);
+    //     return ans;
+    // }
 };
