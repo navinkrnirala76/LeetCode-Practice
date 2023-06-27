28d20a0cf8bcb497983bb8675028ca5e475932af
Navin Kumar <73814698+navinkrnirala76@users.noreply.github.com>
2023-06-27 09:21:02 +0530

    Create Contains Duplicate.cpp

---
+++ Contains Duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for(auto &i: nums){
+            if(s.count(i))return true;
+            s.insert(i);
+        }
+        return false;
+    }
+};
