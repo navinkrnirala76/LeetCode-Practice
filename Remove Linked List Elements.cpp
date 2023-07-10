2101e0648b1336a594581d98bc9223e4ada550ba
Navin Kumar <73814698+navinkrnirala76@users.noreply.github.com>
2023-07-10 11:01:36 +0530

    Create Remove Linked List Elements.cpp

---
+++ Remove Linked List Elements.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val)
+    {   
+        while(head != NULL && head->val == val) // if all the elements are same
+        {
+            head = head->next;
+        }
+
+        if(head == NULL)
+            return NULL;
+        
+        ListNode* pointer;
+        pointer = head;
+
+        while (pointer->next != NULL)  // removing ftom middle of the linked list
+        {
+            if(pointer->next->val == val)
+            {
+                pointer->next = pointer->next->next;
+            }
+            else
+            {
+                pointer = pointer->next;    
+            }
+        }
+        return head;
+    }
+};
