286b8a989f40cdea7fd19aef270039917baaff60
Navin Kumar <73814698+navinkrnirala76@users.noreply.github.com>
2023-07-17 12:26:54 +0530

    Create Fibonacci Number.cpp

---
+++ Fibonacci Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int topdown(int n ,vector<int> &dp){
+        if(n==1||n==0)
+            return n;
+        if(dp[n]!=-1)
+            return dp[n];
+        dp[n]=topdown(n-1,dp)+topdown(n-2,dp);
+        return dp[n];
+    }
+    int fib(int n) {
+        vector<int> dp(n+1,-1);
+        return topdown(n,dp);
+    }
+};
